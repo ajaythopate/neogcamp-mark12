41eea3552e79d78fd790c5ff5447b69d4ca9ce5c
ajaythopate <ajaythopate999@gmail.com>
2022-10-10 08:38:42 +0530

    fix : Is Triangle validations and styles

--- Istriangle.js
+++ Istriangle.js
@@ -10,14 +10,26 @@ function CalculateSumOfAngles(angle1, angle2, angle3) {
  
 
 function IsTriangle() {
-    const SumOfAngles = CalculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
- 
-    if (SumOfAngles === 180) {
-        output.innerText = "yey, The angle from a triangle";
-    }
-    else {
-        output.innerText = "oh oh,The angle don't from a triangle";
-    }
+    if (inputs[0].value === "" || inputs[1].value === "" || inputs[2].value === "") {
+        showMessage("Fields can not be empty.", "red");
+    } else if (inputs[0].value < 0 || inputs[1].value < 0 || inputs[2].value < 0) { 
+        showMessage("Negative numbers not allowed.", "red");
+    } else {
+        const SumOfAngles = CalculateSumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+
+        if (SumOfAngles === 180) {
+            showMessage("yey, The angle from a triangle", "green");
+        }
+        else {
+            showMessage("oh oh,The angle don't from a triangle", "red");
+        }
+    }   
+   
 }
 
- IsTriangleBtn=addEventListener("click",IsTriangle)
+const showMessage = (message, color) => {
+    output.innerText = message;
+    output.style.color = color;
+};
+
+ IsTriangleBtn.addEventListener("click",IsTriangle)
